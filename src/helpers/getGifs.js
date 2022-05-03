15f89a31f2d154d1d670f01e05ffd915bcc68fe8
Javier Zavala <javier.zavala@ksp.com.mx>
2022-05-03 18:24:19 -0500

    Otro Fix que no me dijeron :P

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const  getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=fg0PUlAfu8273Vt3GAZ4HY4Emify6rpC`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=fg0PUlAfu8273Vt3GAZ4HY4Emify6rpC`;
   const resp = await fetch(url);
   const data = await resp.json();
   const dataGif = data.data.map((img) => {
